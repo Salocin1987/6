237d850ec1241cbae903486cd547af602fe20b25
Salocin1987 <72270336+Salocin1987@users.noreply.github.com>
2021-01-14 02:23:11 +0100

    Initial commit

---
+++ qgis2web_2020_12_11-00_48_12_439372/data/Photos_1.js
@@ -0,0 +1,10 @@
+var json_Photos_1 = {
+"type": "FeatureCollection",
+"name": "Photos_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nom": "City Park" }, "geometry": { "type": "Point", "coordinates": [ -90.093952687153532, 29.986337974226757 ] } },
+{ "type": "Feature", "properties": { "id": null, "Nom": "Mississippi RiverLL" }, "geometry": { "type": "Point", "coordinates": [ -90.140642426168441, 29.955607775869545 ] } },
+{ "type": "Feature", "properties": { "id": "1", "Nom": "Fontainebleau State Park" }, "geometry": { "type": "Point", "coordinates": [ -90.043828434853026, 30.335628593995899 ] } }
+]
+}
---
+++ qgis2web_2020_12_11-00_48_12_439372/data/centredintrts_0.js
@@ -0,0 +1,8 @@
+var json_centredintrts_0 = {
+"type": "FeatureCollection",
+"name": "centredintrts_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "def": "Lycée français de la nouvelle Orléans" }, "geometry": { "type": "Point", "coordinates": [ -90.121414913004315, 29.920444006175266 ] } }
+]
+}
